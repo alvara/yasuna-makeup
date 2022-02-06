dea8b265cba0cd650433c09ac23bccdb1c80dc5e
Jesse Alvarado <contact@jessealvarado.com>
2022-02-06 21:21:56 +0900

    setup admin login page

---
+++ src/routes/admin/index.svelte
@@ -0,0 +1,9 @@
+
+<script>
+	import { browser } from '$app/env';
+</script>
+
+{#if browser}
+  <!-- Include the script that builds the page and powers Netlify CMS -->
+  <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
+{/if}
